3c72557636b72d2da11dab14e063e79bd13facd8
Daniel Bright <brahyt@gmail.com>
2019-12-09 13:17:37 -0800

    maze part1

--- mazeEscape.js
+++ mazeEscape.js
@@ -11,3 +11,23 @@ let maze = [
     [' ', '*', '*', '*', '*', '*', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
+
+// need to get to 'e'
+//
+function mazeNav (array, a, b) {
+  console.log(a, b)
+  if(array[a][b] === 'e') return " WIN"
+  if(array[a][b+1] === undefined) return "D" + mazeNav(array, a + 1, b)
+  if(array[a + 1][b] === undefined) return "R" + mazeNav(array, a + 1, b)
+  if(array[a][b+1] !== "*") return "R" + mazeNav(array, a, b + 1)
+  if(array[a + 1][b] !== "*") return "D" + mazeNav(array, a + 1, b)
+}
+
+console.log(mazeNav(mySmallMaze, 0, 0))
+console.log(mazeNav(maze, 0, 0))
+
+// Start at array[0][0]
+// If array[x][i+1] is " " return L + array[0][i+1]
+// If array[x+1][i] is " " return D + array[x+1][i]
+// If array[x][i-1] is " " return R + array[x][i-1]
+
